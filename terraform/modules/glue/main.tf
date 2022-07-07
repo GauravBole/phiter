50fa282588898aad37a6a234f98dbe43934f0c32
gauravbole <gauravbole94@gmail.com>
2022-07-07 17:24:43 +0530

    glue model setup

---
+++ terraform/modules/glue/main.tf
@@ -0,0 +1,37 @@
+resource "aws_glue_job" "glue_job" {
+  name     = var.job_name
+  role_arn = var.glue_role_arn
+
+  worker_type       = var.worker_type
+  number_of_workers = var.number_of_workers
+  max_retries       = var.max_retries
+
+  command {
+    python_version  = 3
+    script_location = ""
+  }
+
+  default_arguments = merge({
+    "--enable-auto-scaling"                   = "true"
+    "--enable-continuous-cloudwatch-log"      = "true"
+    "--enable-continuous-log-filter"          = "true"
+    "--enable-metrics"                        = "true"
+    "--enable-s3-parquet-optimized-committer" = "true"
+    "--enable-spark-ui"                       = "false"
+    "--env"                                   = var.environmanet
+    "--job-bookmark-option"                   = var.job_bookmark
+    "--job-language"                          = "python"
+
+    },
+    var.default_arguments
+  )
+
+  execution_property {
+    max_concurrent_runs = var.max_concurrent_runs
+  }
+
+  tags = merge({
+    Name = var.job_name
+  }, var.tags)
+
+}
---
+++ terraform/modules/glue/variables.tf
@@ -0,0 +1,64 @@
+variable "aws_account_id" {
+
+}
+
+variable "code_bucket" {
+
+}
+
+variable "code_s3_prefix" {
+
+}
+
+variable "default_arguments" {
+  default = {}
+}
+
+variable "environmanet" {
+
+}
+
+variable "glue_role_arn" {
+
+}
+
+variable "job_bookmark" {
+  default = "job-bookmark-disable"
+}
+
+variable "job_name" {
+
+}
+
+variable "max_retries" {
+  default = 0
+}
+
+variable "max_concurrent_runs" {
+  default = 1
+}
+
+variable "number_of_workers" {
+  default = 2
+}
+
+variable "region_name" {
+  default = "eu-west-1"
+}
+
+
+variable "project_name" {
+
+}
+
+variable "script_file" {
+
+}
+
+variable "tags" {
+  default = {}
+}
+
+variable "worker_type" {
+  default = "Standard"
+}
